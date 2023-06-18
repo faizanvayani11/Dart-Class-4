7ad2f2f03e69a598309cfe079f2dda505c0bbd96
faizanvayani11 <mfaizanvayani2000@gmail.com>
2023-06-18 14:04:51 +0500

    ain

---
+++ class4.dart
@@ -0,0 +1,34 @@
+void main() {
+  // List stdNames = [
+  //   "Bilal",
+  //   "ALi ",
+  //   "Ahmed",
+  //   "Kashif",
+  //   "sunil",
+  //   "hahaha",
+  //   "heheheh",
+  //   "shoaib"
+  // ];
+  // print(stdNames[0]);
+  // print(stdNames.length);
+  // print(stdNames.reversed);
+  // print(stdNames[stdNames.length - 2]);
+
+  // stdNames.replaceRange(stdNames.length - 1, stdNames.length, ["Faizan"]);
+  // print(stdNames);
+
+  // List nums = [45, 25, 12, 29, 86, 95, 47, 56, 36, 378, 29, 18, 3, 4, 6, 25];
+  // nums.removeWhere((e) => e > 20);
+  // print(nums);
+
+  Map stdRecord = {"Name": "Faizan", "Idd": "CSC-21S-067", "Marks": "95"};
+  print(stdRecord);
+
+  List Student = [
+    {"Name": "Faizan", "Id": "CSC-21S-067", "Marks": 95},
+    {"Name": "Adeel", "Id": "CSC-21S-009", "Marks": 91},
+    {"Name": "kashif", "Id": "CSC-21S-54", "Marks": 45}
+  ];
+
+  print(Student);
+}
